4b5cfd7a4a9f331336a90f35635ab3ec47d41954
User <lobna.makni@cegelec.com>
2021-09-08 16:28:48 +0200

    xxx

--- src/modules/InputData/InputData.ts
+++ src/modules/InputData/InputData.ts
@@ -229,7 +229,8 @@ console.log("*************************",this.token);
    //console.log(statusCode);
 
 return new Promise((resolve)=>{xml2js.parseString(body,(err:any, result:any)=>{
-const bays=result["SOAP-ENV:Envelope"]["SOAP-ENV:Body"][0]["ns1:getBaysResponse"][0]["baysList"][0];
+const bays=result["SOAP-ENV:Envelope"]["SOAP-ENV:Body"][0]["ns1:getBaysResponse"][0]["baysList"][0]["ns2:Bay"];
+console.log(Object.keys(bays));
 //console.log(JSON.stringify(bays));
 const bay=bays.map((el:any)=> {
   const name=el["ns2:name"][0];
